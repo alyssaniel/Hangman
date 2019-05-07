65932acb33b03623d2fbb3f6bd4f3153f4256ef0
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-05-07 07:38:13 -0400

    Created intellij Project

---
+++ src/com/Lickingheights/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random
+package com.Lickingheights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
