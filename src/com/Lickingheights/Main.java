19f073c433f27d602902164723814fedb6e0cfb9
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-05-09 08:53:42 -0400

    Started the project

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -1,10 +1,30 @@
 import java.util.Scanner;
-import java.util.Random
+import java.util.Random;
 package com.Lickingheights;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int mistakes = 0;
+        int tries = 15;
+
+
+
+
+
+
+
+
+        System.out.println("Welcome to Hangman");
+        System.out.println("");
+
+
+
+
+
+
+
+
     }
 }
