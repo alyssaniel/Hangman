79fed14c7086e41c54833ede9ff5c3ad628465fd
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-05-10 07:41:46 -0400

    Added Ints, Strings, And Scanners

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -6,11 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //ints
         int mistakes = 0;
         int tries = 15;
+        int choice = generator.nextInt(words.length);
 
+        //Scanners
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        Random generator  = new Random();
 
-
+        //Strings
+        String lettersGuessed = null;
+        String lettersWrong = null;
+        String lettersRight = null;
+        String[] words = {"cat","love","programming","updates","grapes"};
+        String secretWord = words[choice];
+        String userGuess = null;
+        System.out.println(secretWord);
 
 
 
