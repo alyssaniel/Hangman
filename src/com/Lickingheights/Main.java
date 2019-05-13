4ac0c177b1140c302878388d8fbb4bebbf6dbc4a
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-05-13 07:34:56 -0400

    Fixed The String Error

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -1,17 +1,12 @@
+package com.Lickingheights;
 import java.util.Scanner;
 import java.util.Random;
-package com.Lickingheights;
+
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-        //ints
-        int mistakes = 0;
-        int tries = 15;
-        int choice = generator.nextInt(words.length);
-
         //Scanners
         Scanner keyboard;
         keyboard = new Scanner(System.in);
@@ -22,16 +17,39 @@ public class Main {
         String lettersWrong = null;
         String lettersRight = null;
         String[] words = {"cat","love","programming","updates","grapes"};
-        String secretWord = words[choice];
         String userGuess = null;
+
+        //ints
+        int mistakes = 0;
+        int tries = 15;
+        int choice = generator.nextInt(words.length);
+
+        String secretWord = words[choice];
+
         System.out.println(secretWord);
 
 
 
 
 
-        System.out.println("Welcome to Hangman");
-        System.out.println("");
+        System.out.println("Welcome to Hangman.");
+        System.out.println("Guess a letter:");
+        userGuess = keyboard.nextLine();
+
+        lettersGuessed += userGuess;
+
+        if(secretWord.indexOf(userGuess)>=0){
+            lettersRight += userGuess;
+        } else {
+            lettersWrong += userGuess;
+        }
+        displayBoard(mistakes);
+        for ( int i = 0 ; i < secretWord.length() ; i++ ) {
+            System.out.print("_ ");
+        }
+
+
+    }
 
 
 
